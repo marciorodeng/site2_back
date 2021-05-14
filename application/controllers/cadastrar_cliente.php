7c87dbbbbdfb4d1b5d0d266ad54d2f57a0f913d9
marciorodeng <marciorodeng@gmail.com>
2021-05-14 08:19:30 -0300

    Limpeza de caracteres - Cliente e Endereço - Funcionando

--- application/controllers/cadastrar_cliente.php
+++ application/controllers/cadastrar_cliente.php
@@ -11,6 +11,21 @@ if($btnCadUsuario){
 	$cadastrar = 0;
 	$dados_st = array_map('strip_tags', $dados_rc);
 	$dados = array_map('trim', $dados_st);
+
+	$caracteres_sem_acento = array(
+		'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+		'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+		'Ï'=>'I', 'Ñ'=>'N', 'N'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+		'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+		'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+		'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'n'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+		'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f',
+		'a'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'A'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S', 'Ț'=>'T',
+	);
+
+	$cliente0 = $dados['NomeCliente'];
+	$cliente1 = preg_replace("/[^a-zA-Z]/", " ", strtr($cliente0, $caracteres_sem_acento));
+	$cliente = trim(mb_strtoupper($cliente1, 'ISO-8859-1'));	
 	
 	if(in_array('',$dados)){
 		//$erro = true;
@@ -193,7 +208,7 @@ if($btnCadUsuario){
 						'5',
 						'1',
 						'CONTA PESSOAL',
-						'" .$dados['NomeCliente']. "',
+						'" .$cliente. "',
 						'" .$dados['CelularCliente']. "',
 						'" .$Codigo. "',
 						'" .$DataCadastroCliente. "',
@@ -219,7 +234,7 @@ if($btnCadUsuario){
 							'" .$idSis_Empresa. "',
 							'1',
 							'" .$id_usuario_5. "',
-							'" .$dados['NomeCliente']. "',
+							'" .$cliente. "',
 							'" .$dados['CelularCliente']. "',
 							'" .$CodInterno. "',
 							'" .$Codigo. "',
--- application/models/finalizar_pedido.php
+++ application/models/finalizar_pedido.php
@@ -78,6 +78,43 @@
 			$referencia = filter_input(INPUT_POST,'Referencia');			
 		}		
 	}
+
+	$caracteres_sem_acento = array(
+		'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+		'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+		'Ï'=>'I', 'Ñ'=>'N', 'N'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+		'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+		'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+		'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'n'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+		'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f',
+		'a'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'A'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S', 'Ț'=>'T',
+	);
+
+	$logradouro = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($logradouro, $caracteres_sem_acento));
+	$logradouro = trim(mb_strtoupper($logradouro, 'ISO-8859-1'));
+
+	$cep = preg_replace("/[^0-9]/", " ", strtr($cep, $caracteres_sem_acento));
+	$cep = trim(mb_strtoupper($cep, 'ISO-8859-1'));
+
+	$numero = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($numero, $caracteres_sem_acento));
+	$numero = trim(mb_strtoupper($numero, 'ISO-8859-1'));
+
+	$complemento = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($complemento, $caracteres_sem_acento));
+	$complemento = trim(mb_strtoupper($complemento, 'ISO-8859-1'));
+
+	$bairro = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($bairro, $caracteres_sem_acento));
+	$bairro = trim(mb_strtoupper($bairro, 'ISO-8859-1'));
+
+	$cidade = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($cidade, $caracteres_sem_acento));
+	$cidade = trim(mb_strtoupper($cidade, 'ISO-8859-1'));
+
+	$estado = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($estado, $caracteres_sem_acento));
+	$estado = trim(mb_strtoupper($estado, 'ISO-8859-1'));
+
+	$referencia = preg_replace("/[^a-zA-Z0-9]/", " ", strtr($referencia, $caracteres_sem_acento));
+	$referencia = trim(mb_strtoupper($referencia, 'ISO-8859-1'));
+	
+	
 	
 	$logradouro1 = trim(mb_strtoupper($logradouro, 'UTF-8'));
 	$complemento1 = trim(mb_strtoupper($complemento, 'ISO-8859-1'));
@@ -180,9 +217,9 @@
 														'".$dataentrega."',
 														'".$hora_mais1."',
 														'".$cep."',
-														'".$logradouro1."',
+														'".$logradouro."',
 														'".$numero."',
-														'".$complemento1."',
+														'".$complemento."',
 														'".$bairro."',
 														'".$referencia."',
 														'".$cidade."',
@@ -475,6 +512,8 @@
 																	ValorComissaoServico,
 																	ValorComissaoCashBack,
 																	DataValidadeProduto,
+																	DataConcluidoProduto,
+																	HoraConcluidoProduto,
 																	ConcluidoProduto,
 																	idTab_TipoRD,
 																	idTab_Modulo,
@@ -499,6 +538,8 @@
 																	'".$sub_total_servico."',
 																	'".$sub_total_cashback."',
 																	'".$dia."',
+																	'".$dataentrega."',
+																	'".$hora_mais1."',
 																	'N',
 																	'2',
 																	'1',
@@ -592,7 +633,7 @@
 			//header("Location: meus_pedidos.php");
 			//echo "<script>alert('Pedido Finalizado')</script>";
 			//echo '<script> window.location = "entrega.php?id='.$id_pedido.'" </script>';			
-
+			
 			if($localpagamento == "O" && ($tipofrete == "1" || $tipofrete == "3")){
 				if($formapagamento == "1" || $formapagamento == "2" || $formapagamento == "3"){
 					echo '<script> window.location = "pagar.php?id='.$id_pedido.'" </script>';
