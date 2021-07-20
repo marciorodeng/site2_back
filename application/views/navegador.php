636a0fed25f28c72d6bfea4cb823228c889df73d
marciorodeng <marciorodeng@gmail.com>
2021-07-19 21:13:46 -0300

    Ajustes nas cores do botão de menu

--- application/views/navegador.php
+++ application/views/navegador.php
@@ -5,33 +5,29 @@
 ?>
 <section id="navegador" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	<nav class="navbar navbar-inverse navbar-fixed-top navbar-menu">
-		<div class="row">
-			<div class="navbar-header">
-				<div class="row">	
-					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-4">
-						<a class="navbar-brand navbar-logo" href="inicial.php"><img src="<?php echo $idSis_Empresa ?>/documentos/miniatura/<?php echo $row_documento['Logo_Nav']; ?>"></a>
-						<!--<span class="navbar-brand "style="color: #FFFFFF" ><?php #echo utf8_encode($row_empresa['NomeEmpresa']);?></span>-->
-					</div>
-					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-4">
-						<?php if(isset($_SESSION['Nome_Cliente'.$idSis_Empresa])){ ?>
-							<a class="navbar-foto" href="">
-								<img class=" img-circle " width='40' src="../<?php echo $row_empresa['Site']; ?>/<?php echo $row_empresa['idSis_Empresa']; ?>/clientes/miniatura/<?php echo $_SESSION['Arquivo_Cliente'.$idSis_Empresa]; ?>" alt="">
-							</a>							
-						<?php } else { ?>
-							<a class="navbar-foto" href="">
-								<img class=" img-circle " width='40' src="../<?php echo $row_empresa['Site']; ?>/<?php echo $row_empresa['idSis_Empresa']; ?>/clientes/miniatura/Foto.jpg" alt="">
-							</a>
-						<?php } ?>
-					</div>
-					<div class="col-lg-1 col-md-1 col-sm-1 col-xs-4">
-						<button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-				</div>
+		<div class="row">	
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-4 navbar-header">
+				<a class="navbar-brand navbar-logo" href="inicial.php"><img src="<?php echo $idSis_Empresa ?>/documentos/miniatura/<?php echo $row_documento['Logo_Nav']; ?>"></a>
+				<!--<span class="navbar-brand "style="color: #FFFFFF" ><?php #echo utf8_encode($row_empresa['NomeEmpresa']);?></span>-->
+			</div>	
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-4">			
+				<?php if(isset($_SESSION['Nome_Cliente'.$idSis_Empresa])){ ?>
+					<a class="navbar-foto" href="">
+						<img class=" img-circle "  width='40' src="../<?php echo $row_empresa['Site']; ?>/<?php echo $row_empresa['idSis_Empresa']; ?>/clientes/miniatura/<?php echo $_SESSION['Arquivo_Cliente'.$idSis_Empresa]; ?>" alt="">
+					</a>							
+				<?php } else { ?>
+					<a class="navbar-foto" href="">
+						<img class=" img-circle "  width='40' src="../<?php echo $row_empresa['Site']; ?>/<?php echo $row_empresa['idSis_Empresa']; ?>/clientes/miniatura/Foto.jpg" alt="">
+					</a>
+				<?php } ?>
+			</div>		
+			<div class="col-xs-4 navbar-header">	
+				<button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 			</div>
 		</div>
 		<div class="row">
